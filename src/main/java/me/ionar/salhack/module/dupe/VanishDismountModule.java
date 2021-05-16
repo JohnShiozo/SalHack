559a0b0838cf3f085780f3003914e88814e9abad
JohnShiozo <66097391+JohnShiozo@users.noreply.github.com>
2021-05-16 18:57:03 -0300

    Update VanishDismountModule.java
    a single digit makes difference

--- src/main/java/me/ionar/salhack/module/dupe/VanishDismountModule.java
+++ src/main/java/me/ionar/salhack/module/dupe/VanishDismountModule.java
@@ -1,4 +1,4 @@
-package me.ionar.salhack.modules.dupe;
+package me.ionar.salhack.module.dupe;
 
 import java.util.function.Predicate;
 import me.ionar.salhack.events.network.EventNetworkPacketEvent;
