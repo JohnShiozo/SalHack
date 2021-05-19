6d8091b3ffa952613ad132c59fe66a34e4a7311d
JohnShiozo <66097391+JohnShiozo@users.noreply.github.com>
2021-05-19 12:08:29 -0300

    Update ModuleManager.java

--- src/main/java/me/ionar/salhack/managers/ModuleManager.java
+++ src/main/java/me/ionar/salhack/managers/ModuleManager.java
@@ -142,7 +142,6 @@ public class ModuleManager
         
         /// Render
         Add(new AntiFog());
-        Add(new BaseTracerModule());
         Add(new BlockHighlightModule());
         Add(new BreakHighlightModule());
         Add(new BrightnessModule());
@@ -165,7 +164,6 @@ public class ModuleManager
         Add(new TracersModule());
         Add(new TrajectoriesModule());
         Add(new ViewClipModule());
-        Add(new ViewModelModule());
         Add(new VoidESPModule());
         Add(new WaypointsModule());
         Add(new MapBoundariesModule());
