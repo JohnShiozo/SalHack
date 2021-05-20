84714a349e4e31616d23531c14c788ac1b0822af
joaoshiozo <joaoshiozo@gmail.com>
2021-05-19 23:54:58 -0300

    oh my

---
+++ src/main/java/me/ionar/salhack/main/SalHackStatic.java
@@ -0,0 +1,12 @@
+package me.ionar.salhack.main;
+
+import me.ionar.salhack.module.combat.*;
+
+public class SalHackStatic
+{
+    // mods
+
+    public static KillAuraModule KillAuraModule;
+    public static AutoCrystalRewrite AutoCrystalRewrite;
+    public static SurroundModule SurroundModule;
+}
