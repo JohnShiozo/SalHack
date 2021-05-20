444acde0704487e721f985cfc1131489a17954ba
joaoshiozo <joaoshiozo@gmail.com>
2021-05-20 14:25:40 -0300

    science

--- src/main/java/me/ionar/salhack/events/MinecraftEvent.java
+++ src/main/java/me/ionar/salhack/events/MinecraftEvent.java
@@ -5,23 +5,36 @@ import me.zero.alpine.fork.event.type.Cancellable;
 
 public class MinecraftEvent extends Cancellable
 {
-    private Era era = Era.PRE;
+    private Stage _stage = Stage.Pre;
     private final float partialTicks;
+    private Era era = Era.PRE;
+
+    public Era getEra()
+    {
+        return era;
+    }
+
 
     public MinecraftEvent()
     {
         partialTicks = Wrapper.GetMC().getRenderPartialTicks();
     }
     
-    public MinecraftEvent(Era p_Era)
+    public MinecraftEvent(Stage stage)
     {
-        partialTicks = Wrapper.GetMC().getRenderPartialTicks();
-        era = p_Era;
+        this();
+        _stage = stage;
     }
 
-    public Era getEra()
+    public Stage getStage()
     {
-        return era;
+        return _stage;
+    }
+
+    public void setEra(Stage stage)
+    {
+        this.setCancelled(false);
+        _stage = stage;
     }
 
     public float getPartialTicks()
@@ -29,11 +42,16 @@ public class MinecraftEvent extends Cancellable
         return partialTicks;
     }
     
+    public enum Stage
+    {
+        Pre,
+        Post,
+    }
+
     public enum Era
     {
         PRE,
         PERI,
-        POST
+        POST,
     }
-
 }
