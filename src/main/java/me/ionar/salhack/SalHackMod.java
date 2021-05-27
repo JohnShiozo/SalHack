7e1bc04959e054a065b61db6cbda0c75ab8569b6
joaoshiozo <joaoshiozo@gmail.com>
2021-05-27 14:24:26 -0300

    fix conflicts

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -24,7 +24,7 @@ public final class SalHackMod
 {
     public static final String NAME = "SalHack";
     public static final String MODID = "salhack";
-    public static final String VERSION = "2.06";
+    public static final String VERSION = "2.07";
 
     public static final Logger log = LogManager.getLogger("sal");
 
