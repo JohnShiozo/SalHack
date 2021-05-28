3cc99629734d1223c4ebb0f4c8035888412bfbc4
JohnShiozo <66097391+JohnShiozo@users.noreply.github.com>
2021-05-27 21:46:38 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/me/ionar/salhack/events/packet/PacketEvent.java
+++ src/main/java/me/ionar/salhack/events/packet/PacketEvent.java
@@ -3,37 +3,42 @@ package me.ionar.salhack.events.packet;
 import me.ionar.salhack.events.MinecraftEvent;
 import net.minecraft.network.Packet;
 
-public class PacketEvent extends MinecraftEvent
-{
+public class PacketEvent
+extends MinecraftEvent {
     private final Packet packet;
 
     public PacketEvent(Packet packet) {
-        super();
         this.packet = packet;
     }
 
     public Packet getPacket() {
-        return packet;
+        return this.packet;
     }
 
-    public static class Receive extends PacketEvent {
-        public Receive(Packet packet) {
+    public static class PostSend
+    extends PacketEvent {
+        public PostSend(Packet packet) {
             super(packet);
         }
     }
-    public static class Send extends PacketEvent {
+
+    public static class PostReceive
+    extends PacketEvent {
+        public PostReceive(Packet packet) {
+            super(packet);
+        }
+    }
+
+    public static class Send
+    extends PacketEvent {
         public Send(Packet packet) {
             super(packet);
         }
     }
 
-    public static class PostReceive extends PacketEvent {
-        public PostReceive(Packet packet) {
-            super(packet);
-        }
-    }
-    public static class PostSend extends PacketEvent {
-        public PostSend(Packet packet) {
+    public static class Receive
+    extends PacketEvent {
+        public Receive(Packet packet) {
             super(packet);
         }
     }
