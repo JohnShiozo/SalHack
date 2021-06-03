9c0ad4358ec2282e3009aa708811630fee9da430
JohnShiozo <66097391+JohnShiozo@users.noreply.github.com>
2021-06-03 15:35:32 -0300

    Update AirJumpModule.java

--- src/main/java/me/ionar/salhack/module/movement/AirJumpModule.java
+++ src/main/java/me/ionar/salhack/module/movement/AirJumpModule.java
@@ -3,22 +3,18 @@ package me.ionar.salhack.module.movement;
 import me.ionar.salhack.module.Module;
 import me.ionar.salhack.events.packet.PacketEvent;
 
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-
 import me.zero.alpine.fork.listener.EventHandler;
 import me.zero.alpine.fork.listener.Listener;
  
-public class AirJumpModule extends Module
-{
-
-	public AirJumpModule()
+public class AirJumpModule extends Module{
+	
+public AirJumpModule()
     {
-        super("AirJump", new String[]
-                { "AirJump" }, "Allows you to jump while in air.", "NONE", -1, ModuleType.MOVEMENT);
+      super("AirJump", new String[]
+      { "AirJump" }, "Allows you to jump while in air.", "NONE", -1, ModuleType.MOVEMENT);
     }
 
-	@EventHandler
+    @EventHandler
     private Listener<PacketEvent> OnSendPacket = new Listener<>(p_Event ->
     {
 		if (mc.player != null) {
